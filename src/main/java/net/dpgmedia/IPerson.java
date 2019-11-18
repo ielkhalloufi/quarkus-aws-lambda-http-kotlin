3ebdaeb82a77ab0ef82a4758a56f43660aabd877
Ilias Elkhalloufi <ilias.elkhalloufi@persgroep.nl>
2019-11-18 17:20:30 +0100

    - initial setup

---
+++ src/main/java/net/dpgmedia/IPerson.java
@@ -0,0 +1,4 @@
+package net.dpgmedia;
+
+public interface IPerson {
+}
---
+++ src/main/java/net/dpgmedia/IPersonImpl.java
@@ -0,0 +1,7 @@
+package net.dpgmedia;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class IPersonImpl implements IPerson {
+}
